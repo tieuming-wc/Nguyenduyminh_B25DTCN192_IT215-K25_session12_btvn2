9aca468214f2bc792f560f4eb77f151f09665899
tieuming-wc <duym5412@gmail.com>
2026-07-08 21:29:46 +0700

    Add files via upload

---
+++ model.PY
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class ShipmentModel(Base):
+    __tablename__ = "shipments"
+    id = Column(Integer, primary_key=True)
+    tracking_number = Column(String(50), unique=True, nullable=False)
+    status = Column(String(50), default="PREPARING")
